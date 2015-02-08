9be90d0e9768b04f5ba6752c274bbce9451d0418
jarbona <jarbona@mail.bradley.edu>
2015-02-08 15:40:00 -0600

    Hide/Show Button
    Was able to get down hide/show button by checking the input that matt
    will be putting. Boom!

--- ClueThree.js
+++ ClueThree.js
@@ -1,20 +1,45 @@
 
 function myFunction() {
 
-    var message, x;
+    var message, x, answer, button;
     message = document.getElementById("message");
+	button = document.getElementById("Next");
     message.innerHTML = "";
     x = document.getElementById("clue").value;
 
-
-	if(x == 10){
-		$('input[type="text"]').focus(function() {
-		$(this).addClass("focus");
-		});
-	}else{
-		$('input[type="text"]').blur(function() {
-		$(this).removeClass("focus");
-		});
-	}
+    try { 
+        x = Number(x);
+        if(x == ""){
 		
+		
+			$('input[type="text"]').focus(function() {
+					$(this).addClass("focus");
+			});
+			
+			button.style.display = "none";
+			
+			
+			throw "is empty";
+		}  
+		
+        if(isNaN(x)){
+		$('input[type="text"]').focus(function() {
+					$(this).removeClass("focus");
+			});
+			
+				
+			throw "is not a number";
+		} 
+        if(x > 10)   throw "is too high";
+        if(x < 5)    throw "is too low";
+    }
+    catch(err) {
+        message.innerHTML = "Input " + err;
+    }
+    finally {
+        document.getElementById("demo").value = "";
+    }
 }
+
+
+	
