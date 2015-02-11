e99b3e26913d25ab5556dcb91748761c47943f4c
jarbona <jarbona@mail.bradley.edu>
2015-02-11 01:01:59 -0600

    Finish Clue One/Almost done with Clue Two
    Adrian.html, Justin.html, Bev.html ,Chris.html ,Samantha.html
    ,Seth.html: Are the 5 question website for each person
    
    trivia.css: style all the previous html
    trivia.js: verified if the answer of the previous html are correct
    ClueOne.html: have the first clue for matt
    ClueOne.css: the css that styles ClueOne html

--- trivia.js
+++ trivia.js
@@ -23,10 +23,90 @@ function myFunction() {
 	
 	sum = 0;
 	
+	if(x1 == 'sloth'){
+	
+		message1.innerHTML ="Congrats!!!!";
+		sum = sum +1;
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "block";
+	}
+	
+	if(x2 == 'carbs cheese hotdog'){
+	
+		message2.innerHTML ="Congrats 2!!!!";
+		sum = sum +1;
+	}
+	
+	if(x3 == 'link'){
+	
+		message3.innerHTML ="Congrats 3!!!!";
+		sum = sum +1;
+	}
+	
+	if(x4 == 'brown'){
+	
+		message4.innerHTML ="Congrats 4!!!!";
+		sum = sum +1;
+	}
+	
+	if(x5 == '1 years'){
+	
+		message5.innerHTML ="Congrats 5!!!!";
+		sum = sum +1;
+	}
+	
+	if(sum == 5){
+		message5.innerHTML ="Congrats 5!!!!";
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "none";
+		var element2 = document.getElementById("QuestionTwo");
+        element2.style.display = "none";
+		var element3 = document.getElementById("QuestionThree");
+        element3.style.display = "none";
+		var element4 = document.getElementById("QuestionFour");
+        element4.style.display = "none";
+		var element5 = document.getElementById("QuestionFive");
+        element5.style.display = "none";
+		var element5 = document.getElementById("Hint");
+        element5.style.display = "block";
+	
+	}
+	
+}
+
+
+
+function myFunction2() {
+	var message1,message2,message3,message4, message5, x1, x2, x3, x4, x5, sum;
+    message1 = document.getElementById("message1");
+	message1.innerHTML = "";
+	
+	message2 = document.getElementById("message2");
+	message2.innerHTML = "";
+	
+	message3 = document.getElementById("message3");
+	message3.innerHTML = "";
+	
+	message4 = document.getElementById("message4");
+	message4.innerHTML = "";
+	
+	message5 = document.getElementById("message5");
+	message5.innerHTML = "";
+	
+	x1 = document.getElementById("Question1").value;
+	x2 = document.getElementById("Question2").value;
+	x3 = document.getElementById("Question3").value;
+	x4 = document.getElementById("Question4").value;
+	x5 = document.getElementById("Question5").value;
+	
+	sum = 0;
+	
 	if(x1 == 'jackie chan'){
 	
 		message1.innerHTML ="Congrats!!!!";
 		sum = sum +1;
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "block";
 	}
 	
 	if(x2 == 'fabian'){
@@ -48,6 +128,388 @@ function myFunction() {
 	}
 	
 	if(x5 == 'infrastructure engineering'){
+	
+		message5.innerHTML ="Congrats 5!!!!";
+		sum = sum +1;
+	}
+	
+	if(sum == 5){
+		message5.innerHTML ="Congrats 5!!!!";
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "none";
+		var element2 = document.getElementById("QuestionTwo");
+        element2.style.display = "none";
+		var element3 = document.getElementById("QuestionThree");
+        element3.style.display = "none";
+		var element4 = document.getElementById("QuestionFour");
+        element4.style.display = "none";
+		var element5 = document.getElementById("QuestionFive");
+        element5.style.display = "none";
+		var element5 = document.getElementById("Hint");
+        element5.style.display = "block";
+	
+	}
+	
+}
+
+
+
+function myFunction3() {
+	var message1,message2,message3,message4, message5, x1, x2, x3, x4, x5, sum;
+    message1 = document.getElementById("message1");
+	message1.innerHTML = "";
+	
+	message2 = document.getElementById("message2");
+	message2.innerHTML = "";
+	
+	message3 = document.getElementById("message3");
+	message3.innerHTML = "";
+	
+	message4 = document.getElementById("message4");
+	message4.innerHTML = "";
+	
+	message5 = document.getElementById("message5");
+	message5.innerHTML = "";
+	
+	x1 = document.getElementById("Question1").value;
+	x2 = document.getElementById("Question2").value;
+	x3 = document.getElementById("Question3").value;
+	x4 = document.getElementById("Question4").value;
+	x5 = document.getElementById("Question5").value;
+	
+	sum = 0;
+	
+	if(x1 == 'wales'){
+	
+		message1.innerHTML ="Congrats!!!!";
+		sum = sum +1;
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "block";
+	}
+	
+	if(x2 == 'super bowl'){
+	
+		message2.innerHTML ="Congrats 2!!!!";
+		sum = sum +1;
+	}
+	
+	if(x3 == 'beast'){
+	
+		message3.innerHTML ="Congrats 3!!!!";
+		sum = sum +1;
+	}
+	
+	if(x4 == 'four'){
+	
+		message4.innerHTML ="Congrats 4!!!!";
+		sum = sum +1;
+	}
+	
+	if(x5 == 'utahss'){
+	
+		message5.innerHTML ="Congrats 5!!!!";
+		sum = sum +1;
+	}
+	
+	if(sum == 5){
+		message5.innerHTML ="Congrats 5!!!!";
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "none";
+		var element2 = document.getElementById("QuestionTwo");
+        element2.style.display = "none";
+		var element3 = document.getElementById("QuestionThree");
+        element3.style.display = "none";
+		var element4 = document.getElementById("QuestionFour");
+        element4.style.display = "none";
+		var element5 = document.getElementById("QuestionFive");
+        element5.style.display = "none";
+		var element5 = document.getElementById("Hint");
+        element5.style.display = "block";
+	
+	}
+	
+}
+
+function myFunction4() {
+	var message1,message2,message3,message4, message5, x1, x2, x3, x4, x5, sum;
+    message1 = document.getElementById("message1");
+	message1.innerHTML = "";
+	
+	message2 = document.getElementById("message2");
+	message2.innerHTML = "";
+	
+	message3 = document.getElementById("message3");
+	message3.innerHTML = "";
+	
+	message4 = document.getElementById("message4");
+	message4.innerHTML = "";
+	
+	message5 = document.getElementById("message5");
+	message5.innerHTML = "";
+	
+	x1 = document.getElementById("Question1").value;
+	x2 = document.getElementById("Question2").value;
+	x3 = document.getElementById("Question3").value;
+	x4 = document.getElementById("Question4").value;
+	x5 = document.getElementById("Question5").value;
+	
+	sum = 0;
+	
+	if(x1 == 'baby ruth'){
+	
+		message1.innerHTML ="Congrats!!!!";
+		sum = sum +1;
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "block";
+	}
+	
+	if(x2 == 'grapefruit'){
+	
+		message2.innerHTML ="Congrats 2!!!!";
+		sum = sum +1;
+	}
+	
+	if(x3 == '25'){
+	
+		message3.innerHTML ="Congrats 3!!!!";
+		sum = sum +1;
+	}
+	
+	if(x4 == '40'){
+	
+		message4.innerHTML ="Congrats 4!!!!";
+		sum = sum +1;
+	}
+	
+	if(x5 == '333s'){
+	
+		message5.innerHTML ="Congrats 5!!!!";
+		sum = sum +1;
+	}
+	
+	if(sum == 5){
+		message5.innerHTML ="Congrats 5!!!!";
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "none";
+		var element2 = document.getElementById("QuestionTwo");
+        element2.style.display = "none";
+		var element3 = document.getElementById("QuestionThree");
+        element3.style.display = "none";
+		var element4 = document.getElementById("QuestionFour");
+        element4.style.display = "none";
+		var element5 = document.getElementById("QuestionFive");
+        element5.style.display = "none";
+		var element5 = document.getElementById("Hint");
+        element5.style.display = "block";
+	
+	}
+	
+}
+
+function myFunction5() {
+	var message1,message2,message3,message4, message5, x1, x2, x3, x4, x5, sum;
+    message1 = document.getElementById("message1");
+	message1.innerHTML = "";
+	
+	message2 = document.getElementById("message2");
+	message2.innerHTML = "";
+	
+	message3 = document.getElementById("message3");
+	message3.innerHTML = "";
+	
+	message4 = document.getElementById("message4");
+	message4.innerHTML = "";
+	
+	message5 = document.getElementById("message5");
+	message5.innerHTML = "";
+	
+	x1 = document.getElementById("Question1").value;
+	x2 = document.getElementById("Question2").value;
+	x3 = document.getElementById("Question3").value;
+	x4 = document.getElementById("Question4").value;
+	x5 = document.getElementById("Question5").value;
+	
+	sum = 0;
+	
+	if(x1 == 'restaruant'){
+	
+		message1.innerHTML ="Congrats!!!!";
+		sum = sum +1;
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "block";
+	}
+	
+	if(x2 == '3'){
+	
+		message2.innerHTML ="Congrats 2!!!!";
+	}
+	
+	if(x3 == 'alabama'){
+	
+		message3.innerHTML ="Congrats 3!!!!";
+		sum = sum +1;
+	}
+	
+	if(x4 == '40'){
+	
+		message4.innerHTML ="Congrats 4!!!!";
+		sum = sum +1;
+	}
+	
+	if(x5 == '333s'){
+	
+		message5.innerHTML ="Congrats 5!!!!";
+		sum = sum +1;
+	}
+	
+	if(sum == 5){
+		message5.innerHTML ="Congrats 5!!!!";
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "none";
+		var element2 = document.getElementById("QuestionTwo");
+        element2.style.display = "none";
+		var element3 = document.getElementById("QuestionThree");
+        element3.style.display = "none";
+		var element4 = document.getElementById("QuestionFour");
+        element4.style.display = "none";
+		var element5 = document.getElementById("QuestionFive");
+        element5.style.display = "none";
+		var element5 = document.getElementById("Hint");
+        element5.style.display = "block";
+	
+	}
+	
+}
+
+
+function myFunction6() {
+	var message1,message2,message3,message4, message5, x1, x2, x3, x4, x5, sum;
+    message1 = document.getElementById("message1");
+	message1.innerHTML = "";
+	
+	message2 = document.getElementById("message2");
+	message2.innerHTML = "";
+	
+	message3 = document.getElementById("message3");
+	message3.innerHTML = "";
+	
+	message4 = document.getElementById("message4");
+	message4.innerHTML = "";
+	
+	message5 = document.getElementById("message5");
+	message5.innerHTML = "";
+	
+	x1 = document.getElementById("Question1").value;
+	x2 = document.getElementById("Question2").value;
+	x3 = document.getElementById("Question3").value;
+	x4 = document.getElementById("Question4").value;
+	x5 = document.getElementById("Question5").value;
+	
+	sum = 0;
+	
+	if(x1 == 'giraffe'){
+	
+		message1.innerHTML ="Congrats!!!!";
+		sum = sum +1;
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "block";
+	}
+	
+	if(x2 == 'walmart'){
+	
+		message2.innerHTML ="Congrats 2!!!!";
+		sum = sum +1;
+	}
+	
+	if(x3 == '25'){
+	
+		message3.innerHTML ="Congrats 3!!!!";
+		sum = sum +1;
+	}
+	
+	if(x4 == '4'){
+	
+		message4.innerHTML ="Congrats 4!!!!";
+		sum = sum +1;
+	}
+	
+	if(x5 == 'jennifer lawrence'){
+	
+		message5.innerHTML ="Congrats 5!!!!";
+		sum = sum +1;
+	}
+	
+	if(sum == 5){
+		message5.innerHTML ="Congrats 5!!!!";
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "none";
+		var element2 = document.getElementById("QuestionTwo");
+        element2.style.display = "none";
+		var element3 = document.getElementById("QuestionThree");
+        element3.style.display = "none";
+		var element4 = document.getElementById("QuestionFour");
+        element4.style.display = "none";
+		var element5 = document.getElementById("QuestionFive");
+        element5.style.display = "none";
+		var element5 = document.getElementById("Hint");
+        element5.style.display = "block";
+	
+	}
+	
+}
+
+function myFunction7() {
+	var message1,message2,message3,message4, message5, x1, x2, x3, x4, x5, sum;
+    message1 = document.getElementById("message1");
+	message1.innerHTML = "";
+	
+	message2 = document.getElementById("message2");
+	message2.innerHTML = "";
+	
+	message3 = document.getElementById("message3");
+	message3.innerHTML = "";
+	
+	message4 = document.getElementById("message4");
+	message4.innerHTML = "";
+	
+	message5 = document.getElementById("message5");
+	message5.innerHTML = "";
+	
+	x1 = document.getElementById("Question1").value;
+	x2 = document.getElementById("Question2").value;
+	x3 = document.getElementById("Question3").value;
+	x4 = document.getElementById("Question4").value;
+	x5 = document.getElementById("Question5").value;
+	
+	sum = 0;
+	
+	if(x1 == 'carrot cake'){
+	
+		message1.innerHTML ="Congrats!!!!";
+		sum = sum +1;
+		var element = document.getElementById("QuestionOne");
+        element.style.display = "block";
+	}
+	
+	if(x2 == 'walmart'){
+	
+		message2.innerHTML ="Congrats 2!!!!";
+		sum = sum +1;
+	}
+	
+	if(x3 == 'Wendys'){
+	
+		message3.innerHTML ="Congrats 3!!!!";
+		sum = sum +1;
+	}
+	
+	if(x4 == '4'){
+	
+		message4.innerHTML ="Congrats 4!!!!";
+		sum = sum +1;
+	}
+	
+	if(x5 == 'kicker of elves'){
 	
 		message5.innerHTML ="Congrats 5!!!!";
 		sum = sum +1;
