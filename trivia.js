219c5de49c8a3b8effd22e557b0896959f9ec19c
jarbona <jarbona@mail.bradley.edu>
2015-02-12 08:42:13 -0600

    ClueTwo
    I added all the hints for Clues Two & messages when he gets it wrong.
    
    Troubles:
    Unsure how to used global variable to records how many right he got all
    around
    
    When I click one button, all them like "press"
    
    My page don't scroll down with the keyboard, i need a mouse
    
    Need to do:
    Add the rules of Clues Two
    The final Clue
    Clue Three

--- trivia.js
+++ trivia.js
@@ -1,6 +1,21 @@
+
+var t_r_answer = 0;
+
+function displayResult(){
+	
+	message1 = document.getElementById("result");
+	message1.innerHTML = "";
+	message1.innerHTML = t_r_answer;
+
+}
+
+
+
 function myFunction() {
+
 	var message1,message2,message3,message4, message5, x1, x2, x3, x4, x5, sum;
-    message1 = document.getElementById("message1");
+    
+	message1 = document.getElementById("message1");
 	message1.innerHTML = "";
 	
 	message2 = document.getElementById("message2");
@@ -21,38 +36,61 @@ function myFunction() {
 	x4 = document.getElementById("Question4").value;
 	x5 = document.getElementById("Question5").value;
 	
+	
 	sum = 0;
 	
-	if(x1 == 'sloth'){
+	if(x1 == 'jackie chan'){
 	
-		message1.innerHTML ="Congrats!!!!";
+		
 		sum = sum +1;
-		var element = document.getElementById("QuestionOne");
-        element.style.display = "block";
+		t_r_answer = t_r_answer + 1;
+		message1.innerHTML ="Congrats!!!!" + t_r_answer;
+		
+			
+    }else{
+	
+		message1.innerHTML ="Yup that is wrong";
 	}
 	
-	if(x2 == 'carbs cheese hotdog'){
+	
+	
+	if(x2 == 'fabian'){
 	
 		message2.innerHTML ="Congrats 2!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message2.innerHTML ="Whoa! You don't care about Adrian's family";
 	}
 	
-	if(x3 == 'link'){
+	if(x3 == 'the ferrari'){
 	
 		message3.innerHTML ="Congrats 3!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message3.innerHTML ="Hurray!!! This damn car is gone. Oh! You go this answer wrong ";
 	}
 	
-	if(x4 == 'brown'){
+	if(x4 == 'after earth'){
 	
 		message4.innerHTML ="Congrats 4!!!!";
 		sum = sum +1;
+	}else{
+	
+		message4.innerHTML ="Hint Hint Hint It was realllllyyyy bad";
 	}
 	
-	if(x5 == '1 years'){
+	if(x5 == 'infrastructure engineering'){
 	
 		message5.innerHTML ="Congrats 5!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message5.innerHTML ="This is realllyyyy hard to spell ";
 	}
 	
 	if(sum == 5){
@@ -73,6 +111,8 @@ function myFunction() {
 	}
 	
 }
+
+
 
 
 
@@ -101,36 +141,59 @@ function myFunction2() {
 	
 	sum = 0;
 	
-	if(x1 == 'jackie chan'){
+	if(x1 == 'sloth'){
 	
-		message1.innerHTML ="Congrats!!!!";
 		sum = sum +1;
-		var element = document.getElementById("QuestionOne");
-        element.style.display = "block";
+		t_r_answer = t_r_answer + 1;
+		message1.innerHTML ="Congrats!!!!" + t_r_answer;
+		
+	}else{
+	
+		message1.innerHTML ="Yikes! If you got this wrong, you better just give up ";
+	
 	}
 	
-	if(x2 == 'fabian'){
+	if(x2 == 'carbs cheese hotdog'){
 	
 		message2.innerHTML ="Congrats 2!!!!";
 		sum = sum +1;
+	}else if(x2 == 'cheese carbs hotdog'){
+	
+		message2.innerHTML ="Congrats 2!!!!";
+		sum = sum +1;
+	
+	}else{
+	
+		message2.innerHTML ="I will admit this one is hard";
 	}
 	
-	if(x3 == 'the ferrari'){
+	if(x3 == 'link'){
 	
 		message3.innerHTML ="Congrats 3!!!!";
 		sum = sum +1;
+	}else{
+		
+		message3.innerHTML ="Obviously I am the better friend because I know this";
+	
 	}
 	
-	if(x4 == 'after earth'){
+	if(x4 == 'brown'){
 	
 		message4.innerHTML ="Congrats 4!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message4.innerHTML ="I remember the day I was crushed when I found out she wasn't a true ginger. ";
 	}
 	
-	if(x5 == 'infrastructure engineering'){
+	if(x5 == 'one year'){
 	
 		message5.innerHTML ="Congrats 5!!!!";
 		sum = sum +1;
+	}else{
+		message5.innerHTML ="This year she got it on time!!!";
+	
 	}
 	
 	if(sum == 5){
@@ -154,8 +217,12 @@ function myFunction2() {
 
 
 
+
+
 function myFunction3() {
+
 	var message1,message2,message3,message4, message5, x1, x2, x3, x4, x5, sum;
+	
     message1 = document.getElementById("message1");
 	message1.innerHTML = "";
 	
@@ -172,10 +239,7 @@ function myFunction3() {
 	message5.innerHTML = "";
 	
 	x1 = document.getElementById("Question1").value;
-	x2 = document.getElementById("Question2").value;
-	x3 = document.getElementById("Question3").value;
-	x4 = document.getElementById("Question4").value;
-	x5 = document.getElementById("Question5").value;
+	
 	
 	sum = 0;
 	
@@ -183,32 +247,50 @@ function myFunction3() {
 	
 		message1.innerHTML ="Congrats!!!!";
 		sum = sum +1;
-		var element = document.getElementById("QuestionOne");
-        element.style.display = "block";
-	}
-	
+	}else(
+		message1.innerHTML ="YOU GOT IT RIGHT! Nah, I am joking is wrong"
+	)
+	x2 = document.getElementById("Question2").value;
+	x3 = document.getElementById("Question3").value;
+	x4 = document.getElementById("Question4").value;
+	x5 = document.getElementById("Question5").value;
 	if(x2 == 'super bowl'){
 	
 		message2.innerHTML ="Congrats 2!!!!";
 		sum = sum +1;
+	}else{
+	
+		message2.innerHTML ="HOW CAN YOU FORGET! You are Camilla Godfather!!";
 	}
 	
 	if(x3 == 'beast'){
 	
 		message3.innerHTML ="Congrats 3!!!!";
 		sum = sum +1;
+	}else{
+	
+		message3.innerHTML ="I am surprised you got this wrong. You should be calling her that. Its that good.";
+	
 	}
 	
-	if(x4 == 'four'){
+	if(x4 == 'three'){
 	
 		message4.innerHTML ="Congrats 4!!!!";
 		sum = sum +1;
+	}else{
+	
+		message4.innerHTML ="Hint! You need more than two hands to count them";
+	
+	
 	}
 	
-	if(x5 == 'utahss'){
+	if(x5 == 'utah'){
 	
 		message5.innerHTML ="Congrats 5!!!!";
 		sum = sum +1;
+	}else{
+	
+		message5.innerHTML ="Hint! Its out of the country";
 	}
 	
 	if(sum == 5){
@@ -259,32 +341,70 @@ function myFunction4() {
 	
 		message1.innerHTML ="Congrats!!!!";
 		sum = sum +1;
-		var element = document.getElementById("QuestionOne");
-        element.style.display = "block";
+	}else{
+		
+		message1.innerHTML ="It is literraly the best candy bar ever, why you got this wrong?";
+	
 	}
 	
 	if(x2 == 'grapefruit'){
 	
 		message2.innerHTML ="Congrats 2!!!!";
 		sum = sum +1;
+		
+	}else{
+		message2.innerHTML ="Eh! There is better fruit out there. Seth have an okay taste.";
 	}
 	
-	if(x3 == '25'){
+	if(x3 == 'twenty five'){
 	
 		message3.innerHTML ="Congrats 3!!!!";
 		sum = sum +1;
+		
+	}else if(x3 == 'twenty six'){
+	
+		message3.innerHTML ="Congrats 3!!!!";
+		sum = sum +1;
+	
+	}else{
+		
+		message3.innerHTML ="Hint! More than anyone else in this group. or I am lying?";
+	
 	}
 	
 	if(x4 == '40'){
 	
 		message4.innerHTML ="Congrats 4!!!!";
 		sum = sum +1;
+		
+	}else if(x4 =='31'){
+	
+		message4.innerHTML ="If it was that obvious it wouldn't be here, Matt";
+	
+	}else{
+	
+		message4.innerHTML ="So many good horror movie, so little time. You should take a break and see one";
 	}
 	
-	if(x5 == '333s'){
+	if(x5 == '333'){
 	
 		message5.innerHTML ="Congrats 5!!!!";
 		sum = sum +1;
+		
+	}else if(x5 < '100'){
+		
+		message5.innerHTML ="Matt you are so cold, you should go get a jacket";
+	
+	}else if(x5 < '200'){
+		
+		message5.innerHTML ="Should I give you a hint? Nah I will let you struggle";
+	
+	}else if(x5 < '300'){
+		
+		message5.innerHTML ="You are so close!!!!!! or are you?";
+	
+	}else{
+		message5.innerHTML ="Do you think he ever sleep? I think, he just constantly watching movies. While he works, sleep, exercise, it never stop";
 	}
 	
 	if(sum == 5){
@@ -335,31 +455,49 @@ function myFunction5() {
 	
 		message1.innerHTML ="Congrats!!!!";
 		sum = sum +1;
-		var element = document.getElementById("QuestionOne");
-        element.style.display = "block";
+		
+	}else{
+	
+		message1.innerHTML ="WRONGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGg";
+	
 	}
 	
-	if(x2 == '3'){
+	if(x2 == 'three'){
 	
 		message2.innerHTML ="Congrats 2!!!!";
+		sum = sum +1;
+		
+	}else{
+	
+		message2.innerHTML ="Oh! The many groupme messages.";
 	}
 	
 	if(x3 == 'alabama'){
 	
 		message3.innerHTML ="Congrats 3!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message3.innerHTML ="You know what, I don't know what state Justin love. Wiscosine?";
 	}
 	
-	if(x4 == '40'){
+	if(x4 == 'u'){
 	
 		message4.innerHTML ="Congrats 4!!!!";
 		sum = sum +1;
+		
+	}else{
+		message4.innerHTML ="I think the books is about wizards and dragon. I am like 3% sure. Trust me this is a good hint.";
 	}
 	
-	if(x5 == '333s'){
+	if(x5 == 'u'){
 	
 		message5.innerHTML ="Congrats 5!!!!";
 		sum = sum +1;
+		
+	}else{
+		message5.innerHTML ="She is a cute dog. Wait he is a cute dog. No. She? He?";
 	}
 	
 	if(sum == 5){
@@ -411,32 +549,47 @@ function myFunction6() {
 	
 		message1.innerHTML ="Congrats!!!!";
 		sum = sum +1;
-		var element = document.getElementById("QuestionOne");
-        element.style.display = "block";
+		
+	}else{
+	
+		message1.innerHTML ="Come on! ITs obvious!";
 	}
 	
 	if(x2 == 'walmart'){
 	
 		message2.innerHTML ="Congrats 2!!!!";
 		sum = sum +1;
+	}else{
+	
+		message2.innerHTML ="Only fine man buy steak from there!";
 	}
 	
-	if(x3 == '25'){
+	if(x3 == 'fender san marino'){
 	
 		message3.innerHTML ="Congrats 3!!!!";
 		sum = sum +1;
+		
+	}else{
+		message3.innerHTML ="Good luck! ";
 	}
 	
-	if(x4 == '4'){
+	if(x4 == 'four'){
 	
 		message4.innerHTML ="Congrats 4!!!!";
 		sum = sum +1;
+		
+	}else{
+		message4.innerHTML ="Too bad you didn't attend!";
 	}
 	
 	if(x5 == 'jennifer lawrence'){
 	
 		message5.innerHTML ="Congrats 5!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message5.innerHTML ="bau chica wau wau";
 	}
 	
 	if(sum == 5){
@@ -487,32 +640,51 @@ function myFunction7() {
 	
 		message1.innerHTML ="Congrats!!!!";
 		sum = sum +1;
-		var element = document.getElementById("QuestionOne");
-        element.style.display = "block";
+
+	}else{
+	
+		message1.innerHTML ="We should tell Samantha to make bananna cake!";
 	}
 	
-	if(x2 == 'walmart'){
+	if(x2 == 'woodhollwow'){
 	
 		message2.innerHTML ="Congrats 2!!!!";
 		sum = sum +1;
+	}else{
+		
+		message2.innerHTML ="Apparently he actually lived in a apartment complex.";
 	}
 	
-	if(x3 == 'Wendys'){
+	if(x3 == 'wendy'){
 	
 		message3.innerHTML ="Congrats 3!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message3.innerHTML ="The will get marry one day!";
+		
 	}
 	
-	if(x4 == '4'){
+	if(x4 == 'bloody rabbit'){
 	
 		message4.innerHTML ="Congrats 4!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message4.innerHTML ="This was hilarious!";
+	
 	}
 	
 	if(x5 == 'kicker of elves'){
 	
 		message5.innerHTML ="Congrats 5!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message5.innerHTML ="Yikes! Another hard question";
 	}
 	
 	if(sum == 5){
@@ -564,32 +736,57 @@ function myFunction8() {
 	
 		message1.innerHTML ="Congrats!!!!";
 		sum = sum +1;
-		var element = document.getElementById("QuestionOne");
-        element.style.display = "block";
+		
+	}else{
+		
+		message1.innerHTML ="Come on Matt, what cat name I am thinking?";
+	
 	}
 	
-	if(x2 == '1'){
+	if(x2 == 'one'){
 	
 		message2.innerHTML ="Congrats 2!!!!";
 		sum = sum +1;
+	}else{
+		
+		message2.innerHTML ="Wait isn't he a bass player?";
+	
 	}
 	
-	if(x3 == '23 11'){
+	if(x3 == 'Twenty three november'){
 	
 		message3.innerHTML ="Congrats 3!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message3.innerHTML ="If he only was a girl, he wouldn't gotten that ticket";
+		
 	}
 	
 	if(x4 == '803'){
 	
 		message4.innerHTML ="Congrats 4!!!!";
 		sum = sum +1;
+		
+	}else{
+	
+		message4.innerHTML ="You are so close!!!!";
+		
 	}
 	
 	if(x5 == '1000'){
 	
 		message5.innerHTML ="Congrats 5!!!!";
 		sum = sum +1;
+		
+	}else if (x5 < '500'){
+	
+		message5.innerHTML ="This is embarrassing";
+	
+	}else {
+	
+		message5.innerHTML ="Hint Time! Nah I don't feel like it";
 	}
 	
 	if(sum == 5){
